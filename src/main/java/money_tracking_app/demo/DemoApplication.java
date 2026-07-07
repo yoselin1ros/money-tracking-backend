98a8f90c4051cfcb24a7c8b77eba703ba6b45a7d
Yocelin <yoselin_ros@hotmail.com>
2026-07-06 23:53:32 -0400

    Initial project

---
+++ src/main/java/money_tracking_app/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package money_tracking_app.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
